278747e1d8b234ddd1813a099303d01a8bf09866
vframbach <vframbach@gmail.com>
2015-11-08 11:31:52 -0800

    update starting and ending points to percentage

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
 $(document).ready(function() {
 	var player1 = {
-		position: 0,
+		position: 10,
 		wins: 0,
 		name: null
 	};
 	var player2 = {
-		position: 0,
+		position: 10,
 		wins: 0,
 		name: null
 	};
 
 	var isGameStarted = false;
 	var gameWinner = false;
-	var gameWidth = 1120;
+	var gameWidth = 100;
 
 // key up after key press signals movement
 // player 1 is X (key code 88), player 2 is O (key code 79)
@@ -22,17 +22,17 @@ $(document).ready(function() {
     	}
     	console.log(e);
     	if (e.keyCode == 88) {
-    		player1.position += 40;
-        	$('.box').animate({left: player1.position},80);
+    		player1.position += 4;
+        	$('.box').animate({left: player1.position+'%'},80);
         } else if  (e.keyCode == 79) {	
-        	player2.position += 40;
-        	$('.other-box').animate({left: player2.position},80);
+        	player2.position += 4;
+        	$('.other-box').animate({left: player2.position+'%'},80);
         }
 
 // determines winner
-        if (player1.position >= gameWidth) {
+        if (player1.position >= 90) {
         	gameWinner = player1;
-        } else if (player2.position >= gameWidth) {
+        } else if (player2.position >= 90) {
         	gameWinner = player2;
         }
 
@@ -62,12 +62,12 @@ $(document).ready(function() {
 
 // reset button returns both players to start line and button switches to start button
     $( '.reset-button' ).click(function() {
-    	player1.position = 0;
-		player2.position = 0;
+    	player1.position = 10;
+		player2.position = 10;
     	isGameStarted = false;
 		gameWinner = false;
-    	$('.box').animate({left: player1.position},80);
-    	$('.other-box').animate({left: player2.position},80);
+    	$('.box').animate({left: player1.position+'%'},80);
+    	$('.other-box').animate({left: player2.position+'%'},80);
     	$('.start-button').show();
     	$('.reset-button').hide();
     });
